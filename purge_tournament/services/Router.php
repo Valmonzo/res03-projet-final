47d072cdbb3ae81da170aa59cbabf9584729454d
Valmonzo <valmontpehautpietri@ip-172-31-24-31.eu-west-3.compute.internal>
2023-03-08 20:43:37 +0000

    Adding files , first version of the router

--- purge_tournament/services/Router.php
+++ purge_tournament/services/Router.php
@@ -26,41 +26,147 @@ class Router {
 
     function checkRoute() : void
     {
-        if(isset($_GET['path'])){
-            $route = explode("/", $_GET['path']);
+        $post = $_POST;
+
+        if (!isset($_GET['path'])) {
+
+            $this->rendererController->visitorHome();
+        }
+
+        else {
+
+            $route = explode("/", $_GET['path']); // Je sépare tout ce qui se trouve entre les "/" pour les différentes routes
+
 
             //Pages publiques
 
-            if ($route[0] === "home") {
-                $this->rendererController->visitorHome();
-            }
+            if ($route[0] === "about-us") {
 
-            else if ($route[0] === "about-us") {
-                $this->rendererController->visitorAboutUs();
+                $this->rendererController->visitorAboutUs(); // Qui affichera la page about-us
             }
 
             else if ($route[0] === "schedule") {
 
                 if(isset($route[1])) {
-                    $this->rendererController->renderEvent($route[1]);
+
+                    $this->rendererController->renderEvent($route[1]); // Si je veux afficher un évent en particulier , je prend son id et j'appelle renderEvent
                 }
 
                 else {
-                    $this->rendererController->visitorSchedule();
+
+                    $this->rendererController->visitorSchedule(); // J'affiche la page du Schedule
+                }
+
+            else if($route[0] === "contact") {
+
+                if(isset($route[1]) && $route[1] === "new") {
+
+                    $this->contactController->newContact($post) // J'appelle le contactController pour recevoir le formulaire et traiter la données puis Render avec un msg
+                }
+
+                else {
+
+                    $this->rendererController->visitorContact(); // J'affiche la page de contact
                 }
 
             }
 
 
+            // Pages admin
+
+            else if($route[0] === "admin") {
+
+                if(isset($_SESSION['admin']) && $_SESSION['admin'] === "ok") {
+                    // Je suis connecté
+
+                    if (!isset($route[1])) {
+                        // /admin
+                        $this->rendererController->adminHome(); // J'affiche l'index admin
+                    }
+
+                    else {
+
+                        if(!isset($route[2])) {
+
+                                //  admin/un-truc
+
+                            match ($route[1]) {
+                            'events' => $this->tournamentController->renderEvents(), // J'affiche tous les events /admin/events
+                            'brackets' =>$this->matchController->renderBrackets(), // J'affiche tous les brackets par tournoi /admin/brackets
+                            'teams' => $this->teamController->renderTeams(), // J'affiche toutes les teams /admin/teams
+                            'contacts' => $this->contactController->renderMessages(), // J'affiche toutes les demandes de contact /admin/contacts
+                            default => $this->rendererController->page404(), // Si le chemin est mauvais je redirige sur 404 /page404
+
+                            }
+                        }
+
+                        else {
+
+                            if(!isset($route[3])) {
+                                // admin/un-truc/un-autre
+
+                                if($route[2] === "create" ) {
+                                    match ($route[1]) {
+                                        'events' => $this->tournamentController->createEvents(), // J'affiche  /admin/event/create
+                                        'brackets' =>$this->matchController->createBrackets(), // J'affiche  /admin/bracket/create
+                                        'teams' => $this->teamController->createTeams(), // J'affiche  /admin/team/create
+                                        default => $this->rendererController->page404(), // Si le chemin est mauvais je redirige sur 404 /page404
+                                }
+
+                                else {
+                                    match ($route[1]) {
+                                        'events' => $this->tournamentController->showEvent($route[2]), // J'affiche  /admin/event/:id
+                                        'brackets' =>$this->matchController->showBracket($route[2]), // J'affiche  /admin/bracket/:id
+                                        'teams' => $this->teamController->showTeam($route[2]), // J'affiche  /admin/team/:id
+                                        default => $this->rendererController->page404(), // Si le chemin est mauvais je redirige sur 404 /page404
+                                }
+                            }
+
+                            else {
+
+                                if(!isset($route[4])) {
+
+                                    if($route[3] === "edit") {
+                                        match ($route[1]) {
+                                        'events' => $this->tournamentController->editEvent($route[2]), // J'affiche  /admin/event/:id/edit
+                                        'brackets' =>$this->matchController->editBracket($route[2]), // J'affiche  /admin/bracket/:id/edit
+                                        'teams' => $this->teamController->editTeam($route[2]), // J'affiche  /admin/team/:id/edit
+                                        default => $this->rendererController->page404(), // Si le chemin est mauvais je redirige sur 404 /page404
+                                    }
+
+                                    else if ($route[3] === "delete") {
+                                        match ($route[1]) {
+                                        'events' => $this->tournamentController->deleteEvent($route[2]), // J'affiche  /admin/event/:id/delete
+                                        'brackets' =>$this->matchController->deleteBracket($route[2]), // J'affiche  /admin/bracket/:id/delete
+                                        'teams' => $this->teamController->deleteTeam($route[2]), // J'affiche  /admin/team/:id/delete
+                                        default => $this->rendererController->page404(), // Si le chemin est mauvais je redirige sur 404 /page404
+                                    }
+
+                                    else {
+                                        $this->rendererController->page404(); // Si le chemin est mauvais je redirige sur 404 /page404
+                                    }
+                                }
+                                
+                                else {
+                                    $this->rendererController->page404(); // Si le chemin est mauvais je redirige sur 404 /page404
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+            }
+
+            // Si rien ne rentre dans les conditions
+
+            else {
+
+                $this->rendererController->page404(); // J'affiche une page 404
+            }
+
+
         }
-
-        else {
-
-        }
-
-
-
-
 
     }
 }
