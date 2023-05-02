1ca9f430ca8eec76bf2d930366fab007b329eda0
Valmonzo <valmontpehautpietri@ip-172-31-30-158.eu-west-3.compute.internal>
2023-05-02 17:48:59 +0000

    fix(general): responsive and accessibility

--- purge_tournament/assets/js/schedule.js
+++ purge_tournament/assets/js/schedule.js
@@ -2,6 +2,7 @@ function loadTodayTournament() {
     fetch('https://valmontpehautpietri.sites.3wa.io/res03-projet-final/purge_tournament/loadTodayTournaments')
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             if (data.length > 0) {
                 for (let i = 0; i < data.length; i++) {
                     // Récupération des données du tournoi du jour
@@ -17,7 +18,7 @@ function loadTodayTournament() {
                     let p3 = document.createElement('p');
                     p3.textContent = `Date : ${tournament.date}`;
                     let a = document.createElement('a');
-                    a.href = tournament.stream_url;
+                    a.href = tournament.streamURL;
                     a.textContent = 'Regarder le stream';
                     let bracket = document.createElement('a');
                     bracket.href = `/res03-projet-final/purge_tournament/schedule/${tournament.id}`;
